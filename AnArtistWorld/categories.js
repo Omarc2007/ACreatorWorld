92014c1c1963e55bea32d678afb1857abee74049
Omarc2007 <103296206+Omarc2007@users.noreply.github.com>
2023-11-05 13:52:43 -0800

    Add files via upload

---
+++ AnArtistWorld/categories.js
@@ -0,0 +1,40 @@
+const categories = [
+    {
+        name:"Graphic Design and Digital Art",
+        description:"Digital art is a form of art that involves the use of digital tools to produce images and videos. Meanwhile, graphic design is a profession that combines artwork, text, and typography for use in advertising, web design, and other forms of communication.",
+        skills: "Creativity, Color Theory, Attention to Detail, Collaboration",
+        products: "DesignWizard, Lunacy, Canva, Adobe Spark, Krita"
+    },
+    {
+        name:"Digital Audio Work Station",
+        description: "A digital audio workstation (DAW) is an electronic device or application software used for recording, editing and producing audio files.",
+        skills: "Audio Recording, MIDI Sequencing, Audio Editing, Mixing, Automation",
+        products: "Garage Band, Audacity, Waveform Free, MPC Beats"
+    },
+    {
+        name:"Video Editing",
+        description: "Video editing is the post-production and arrangement of video shots.",
+        skills: "Basic Video Editing Techniques, Audio Editing, Graphics, Keyframing",
+        products: "Apple Imovie, CapCut, CyberLink PowerDirector 365, Adobe Premiera Pro"
+    },
+    {
+        name: "Dance Choreography and Visualization",
+        description: "The art of composing ballets and other dances and planning and arranging the movements, steps, and patterns of dancers.",
+        skills:"Dance Technique, Creativity, Music Apperaciation, Dance Composition",
+        products: "StageKeep, Jackrabbit Dance, COompuDance, Mindbody"
+    },
+    {
+        name: "Sculpture and 3D Modeling",
+        description: "Digital sculpting, also known as sculpt modeling or 3D sculpting, is the use of software that offers tools to push, pull, smooth, grab, pinch or otherwise manipulate a digital object as if it were made of a real-life substance such as clay.",
+        skills: "Spaital Awareness, Material Knowledge, Sculpting Techniques, Conceptual Thinking, 3D Software Proficiency",
+        products: "Blender, Sculptris, Meshmixer, Tinkercad, FreeCAD"
+    },
+];
+
+
+const category1 = categories[0];
+const category2 = categories[1];
+const category3 = categories[2];
+const category4 = categories[3];
+const category5 = categories[4];
+
